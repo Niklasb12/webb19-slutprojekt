44de044f3ba6fe4dd91d45030a3147fbe40dfcff
mandypand <amandajakobsson@mac.local>
2020-11-03 16:36:01 +0100

    some tweaks to widget

---
+++ order-widget.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Order widget
+*/
+
+ class ow_widget extends WP_Widget{
+    public $order_widget;
+    function __construct(){
+
+        parent::__construct(
+            // Base ID of your widget
+            'ow_order', 
+            // Widget name will appear in UI
+            'Order widget',
+            // Widget description
+            array('description' =>'order widget') 
+        );
+
+    }
+
+    function widget($args, $instance){
+        echo $args['before_widget'];
+        global $wpdb;
+        $id = get_the_ID(); 
+
+        "<form method=POST>
+        <button> Order </button>
+        <input type=hidden name=order value=$id></input>
+        </form>"; 
+
+        
+        echo $args['after_widget'];
+    }
+    
+     function form($instance) {
+        printf('<input type="text" name="%s" value="' . $instance["title"] . '">',
+        $this->get_field_name("title")
+        );
+     }
+
+}
+    function ow_init_widget(){
+        register_widget('ow_widget');
+    }
+    add_action('widgets_init', 'ow_init_widget');
+?>
