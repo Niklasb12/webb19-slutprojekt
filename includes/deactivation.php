d038cbba7ddb94cbbccd129076840d3829a22f92
Niklasb12 <niklasborg12@gmail.com>
2020-11-05 13:06:16 +0100

    second

---
+++ includes/deactivation.php
@@ -0,0 +1,14 @@
+<?php
+
+function deactivation() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "add_to_cart";
+    $table_name_order = $wpdb->prefix . "order";
+    $table_name_order_post = $wpdb->prefix . "order_post";
+    $wpdb->query("DROP TABLE IF EXISTS $table_name");
+    $wpdb->query("DROP TABLE IF EXISTS $table_name_order");
+    $wpdb->query("DROP TABLE IF EXISTS $table_name_order_post");
+}
+
+
+register_deactivation_hook(__FILE__, 'deactivation');
---
+++ includes/meta_boxes.php
@@ -0,0 +1,37 @@
+<?php
+
+function save_products($product_id){
+    if(!empty($_POST['price'])){
+        if($_POST['price'] < 0) {
+            update_post_meta($product_id, 'price', 300);
+        }elseif(!is_numeric($_POST['price'])){
+            update_post_meta($product_id, 'price', 300);
+        }else{
+            update_post_meta($product_id, 'price', $_POST['price']);
+        }
+
+    }else{
+        update_post_meta($product_id, 'price', 300);
+    }
+}
+
+function post_meta_box_price($product){
+    printf('<input type="text" name="price" value="%s">',
+    get_post_meta($product->ID, 'price', true )
+    );
+}
+
+function adding_the_meta_boxes(){
+    add_meta_box(
+        "product_price",
+        "Price",
+        "post_meta_box_price",
+        "cpt_order"
+    );
+}
+
+
+
+add_action('save_post', 'save_products');
+
+add_action('add_meta_boxes', 'adding_the_meta_boxes');
