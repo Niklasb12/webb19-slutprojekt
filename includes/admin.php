8236fbad078a7118ef659936b4553f96e03c0bb4
mandypand <amandajakobsson@mac.local>
2020-11-06 09:32:03 +0100

    admin function

--- includes/admin.php
+++ includes/admin.php
@@ -21,25 +21,56 @@
         echo "<h1> Orders </h1>";
         $results_orders = $wpdb->get_results("SELECT * FROM wp_order ORDER BY wp_order.order_date DESC");
 
+    
+
         foreach($results_orders as $order){
-            echo "<div style='display: flex;'><div style='display: flex; flex-direction: column; margin: 40px;'>";
+            echo "<div style='display: flex;'>
+                    <div style='display: flex; flex-direction: column; margin: 40px;'>";
             echo $order->order_date . "<br>" . $order->order_status . " <br>";
 
+            
+            
             echo "
-                <select>
-                    <option value=recieved> recieved </option>
-                    <option value=canceled> canceled </option>
-                    <option value=shipped> shipped </option>
-                    <option value=delivered> delivered </option>
-                </select> <br>
-                <form method=POST>
-                    <input name=id type=hidden value=$order->id>
-                    <button name=save>Save</button>
-                </form>
-                </div>
-                ";
+                    <form method=POST>
+                        <select name=select_order_status>
+                            <option value=recieved> recieved </option>
+                            <option value=canceled> canceled </option>
+                            <option value=shipped> shipped </option>
+                            <option value=delivered> delivered </option>
+                        </select> <br>                                      
+                        <input name=id type=hidden value=$order->id>
+                        <button name=save>Save</button>
+                    </form>
+                </div>";
+
+                
+                if(isset($_POST['id'])){
+                    
+
+                    $select_option = $_POST['select_order_status'];
+                    
+                    $wpdb->query("UPDATE wp_order
+                    SET order_status = 'hårdkodat'");
+           
+           
+                }
+                
+              
 
             }
+            echo $select_option ;
     }
+
+    // function input_status(){
+    //     global $wpdb;
+    //     if(isset($_POST['id'])){
+                
+    //      $wpdb->query("UPDATE wp_order
+    //      SET order_status =$order->status");
+
+
+    //     }
+    // }
+    // add_action('init', 'input_status');
 
 ?>
